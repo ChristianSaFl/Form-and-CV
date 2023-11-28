35fa31fda91920630cad2907a88e30894205d8a4
ChristianSaFl <88692429+ChristianSaFl@users.noreply.github.com>
2023-11-28 00:09:00 -0500

    Add files via upload

---
+++ CvPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using Xamarin.Forms;
+
+namespace TuProyecto
+{
+    public partial class CvPage : ContentPage
+    {
+        public CvPage(string nombre, string apellido, string fechaNacimiento, string correo, string telefono, string dni)
+        {
+            InitializeComponent();
+
+            nombreLabel.Text = $"Nombre: {nombre}";
+            apellidoLabel.Text = $"Apellido: {apellido}";
+            fechaNacimientoLabel.Text = $"Fecha de Nacimiento: {fechaNacimiento}";
+            correoLabel.Text = $"Correo: {correo}";
+            telefonoLabel.Text = $"Teléfono: {telefono}";
+            dniLabel.Text = $"DNI: {dni}";
+        }
+    }
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using Xamarin.Forms;
+
+namespace TuProyecto
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void GuardarDatos(object sender, System.EventArgs e)
+        {
+            string nombre = nombreEntry.Text;
+            string apellido = apellidoEntry.Text;
+            string fechaNacimiento = fechaNacimientoEntry.Text;
+            string correo = correoEntry.Text;
+            string telefono = telefonoEntry.Text;
+            string dni = dniEntry.Text;
+
+            // Validar que los campos obligatorios no estén vacíos
+            if (string.IsNullOrWhiteSpace(nombre) || string.IsNullOrWhiteSpace(apellido) || string.IsNullOrWhiteSpace(fechaNacimiento) || string.IsNullOrWhiteSpace(correo) || string.IsNullOrWhiteSpace(telefono) || string.IsNullOrWhiteSpace(dni))
+            {
+                DisplayAlert("Error", "Por favor, complete todos los campos.", "OK");
+                return;
+            }
+
+            // Navegar a la página de currículum vitae y pasar los datos
+            Navigation.PushAsync(new CvPage(nombre, apellido, fechaNacimiento, correo, telefono, dni));
+        }
+    }
+}
